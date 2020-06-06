a50fe8d14f90d1e8002bb036c617491395607898
kaage <kateageage@gmail.com>
2020-06-06 16:14:49 +0900

    First commit

---
+++ main.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var app = express();
+var fetch = require("node-fetch");
+var jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+app.get("/atcoder", async function (req, res) {
+  res.status(200);
+  res.header({ "Content-Type": "application/json" });
+  let result;
+  await fetch(`https://atcoder.jp/users/define`)
+    .then((response) => {
+      return response.text();
+    })
+    .then((text) => {
+      const dom = new JSDOM(text);
+      result = {
+        rating: dom.window.document.querySelectorAll(".dl-table")[1].children[0]
+          .children[1].children[1].children[0].innerHTML,
+      };
+    });
+  res.json(result);
+  res.end();
+});
+
+app.listen(3000);
+
+console.log("Server is listening");
