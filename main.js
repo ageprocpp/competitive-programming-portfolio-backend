2aaa717c138f686d00a5a71bf726c613182d6ede
kaage <kateageage@gmail.com>
2020-06-07 13:47:29 +0900

    Implemented about the case when the user does not exist and has not participated

--- main.js
+++ main.js
@@ -1,48 +1,84 @@
-var express = require("express");
-var app = express();
-var fetch = require("node-fetch");
-var jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+var express = require("express")
+var app = express()
+var fetch = require("node-fetch")
+var jsdom = require("jsdom")
+const { JSDOM } = jsdom
 
 app.get("/atcoder", async function (req, res) {
-  res.status(200);
-  res.header({
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
-  });
-  let result;
-  await fetch(`https://atcoder.jp/users/${req.query.username}`)
-    .then((response) => {
-      return response.text();
+    res.header({
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
     })
-    .then((text) => {
-      const dom = new JSDOM(text);
-      result = {
-        rating: dom.window.document.querySelectorAll(".dl-table")[1].children[0]
-          .children[1].children[1].children[0].innerHTML,
-        ratingRank: dom.window.document.querySelectorAll(".dl-table")[1]
-          .children[0].children[0].children[1].innerHTML,
-        participatedRatedContests: dom.window.document.querySelectorAll(
-          ".dl-table"
-        )[1].children[0].children[3].children[1].innerHTML,
-      };
-    });
-  await fetch(`https://atcoder.jp/users/${req.query.username}/history`)
-    .then((response) => {
-      return response.text();
-    })
-    .then((text) => {
-      const dom = new JSDOM(text);
-      result = {
-        ...result,
-        participatedContests:
-          dom.window.document.querySelectorAll(".text-center").length - 7,
-      };
-    });
-  res.json(result);
-  res.end();
-});
+    let result
+    let responseSended = false
+    await fetch(`https://atcoder.jp/users/${req.query.username}`)
+        .then(response => {
+            if (!response.ok) {
+                res.status(404)
+                res.end()
+                responseSended = true
+            }
+            return response.text()
+        })
+        .then(text => {
+            if (
+                text.match(/This user has not competed in a rated contest yet./)
+            ) {
+                result = {
+                    rating: 0,
+                    ratingRank: "",
+                    participatedRatedContests: 0,
+                }
+            } else {
+                const dom = new JSDOM(text)
+                result = {
+                    ...result,
+                    rating: dom.window.document.querySelectorAll(".dl-table")[1]
+                        .children[0].children[1].children[1].children[0]
+                        .innerHTML,
+                    ratingRank: dom.window.document.querySelectorAll(
+                        ".dl-table"
+                    )[1].children[0].children[0].children[1].innerHTML,
+                    participatedRatedContests: dom.window.document.querySelectorAll(
+                        ".dl-table"
+                    )[1].children[0].children[3].children[1].innerHTML,
+                }
+            }
+        })
+        .catch(err => console.error(err))
+    await fetch(`https://atcoder.jp/users/${req.query.username}/history`)
+        .then(response => {
+            if (!response.ok) {
+                res.status(404)
+                res.end()
+                responseSended = true
+            }
+            return response.text()
+        })
+        .then(text => {
+            console.log(text)
+            if (text.match(/This user has not competed yet./)) {
+                result = {
+                    ...result,
+                    participatedContests: 0,
+                }
+            } else {
+                const dom = new JSDOM(text)
+                result = {
+                    ...result,
+                    participatedContests:
+                        dom.window.document.querySelectorAll(".text-center")
+                            .length - 7,
+                }
+            }
+        })
+        .catch(err => console.error(err))
+    if (!responseSended) {
+        res.json(result)
+        res.end()
+    }
+})
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000)
 
-console.log("Server is listening");
+console.log("Server is listening")
